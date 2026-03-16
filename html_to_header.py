c2967c26217bccae05cb9ccf60578853ddfc2bbf
nazmuzChakib <chakibnazmuz@gmail.com>
2026-03-16 08:35:53 +0600

    feat: Add sensor integration (DHT, VAC) and update heartbeat to push sensor data to Firebase.

--- html_to_header.py
+++ html_to_header.py
@@ -7,11 +7,11 @@ INPUT_HTML = 'index.html'
 OUTPUT_HEADER = 'WebPage.h'    
 
 def minify_html(html_str):
-    # HTML কমেন্ট মুছে ফেলা
+    # Remove HTML Comments
     html_str = re.sub(r'', '', html_str, flags=re.DOTALL)
-    # ট্যাগের মাঝখানের অপ্রয়োজনীয় স্পেস মুছে ফেলা
+    # remove unnecessery spaching
     html_str = re.sub(r'>\s+<', '><', html_str)
-    # একাধিক স্পেস বা নিউলাইনকে একটি মাত্র স্পেসে রূপান্তর করা
+    # make a single spech from multi newline
     html_str = re.sub(r'\s{2,}', ' ', html_str)
     return html_str.strip()
 
@@ -20,35 +20,35 @@ def compress_and_convert():
         print(f"Error: '{INPUT_HTML}' file not found!")
         return
 
-    # UTF-8 এনকোডিংয়ে HTML ফাইলটি পড়া (ইমোজির জন্য জরুরি)
+    # UTF-8 Enchoding for emoji and other UNICODE supports
     with open(INPUT_HTML, 'r', encoding='utf-8') as f:
         original_html = f.read()
 
     original_size = len(original_html.encode('utf-8'))
 
-    # Minify করা
+    # Minify
     minified_html = minify_html(original_html)
     minified_bytes = minified_html.encode('utf-8')
     minified_size = len(minified_bytes)
 
-    # Gzip কম্প্রেশন করা
+    # Gzip compression
     compressed_data = gzip.compress(minified_bytes)
     compressed_size = len(compressed_data)
     
-    # বাইনারি ডেটাকে Hexadecimal C Array তে কনভার্ট করা এবং সাজানো (প্রতি লাইনে ১৬টি বাইট)
+    # Convert binary data to Hexadecimal C Array(16 byte each line)
     hex_list = [f"0x{b:02X}" for b in compressed_data]
     formatted_hex = ""
     for i in range(0, len(hex_list), 16):
         line = ", ".join(hex_list[i:i+16])
         formatted_hex += f"    {line},\n"
     
-    # শেষের কমা এবং নিউলাইন মুছে ফেলা
+    # Remove comma's and unnecessery new line
     formatted_hex = formatted_hex.rstrip(",\n")
     
-    # বর্তমান সময় (Timestamp)
+    # (Timestamp)
     current_time = datetime.now().strftime("%Y-%m-%d %I:%M:%S %p")
 
-    # হেডার ফাইলের স্ট্রাকচার তৈরি করা
+    # Header file structure for CypherNode_PRO projectrs
     header_content = f"""/**
  * @file {OUTPUT_HEADER}
  * @brief Auto-generated Minified & Gzipped HTML file
@@ -71,7 +71,7 @@ const size_t index_html_gz_len = {compressed_size};
 #endif // WEBPAGE_H
 """
 
-    # হেডার ফাইলটি সেভ করা
+    # Save header file on project location
     with open(OUTPUT_HEADER, 'w', encoding='utf-8') as f:
         f.write(header_content)
 
